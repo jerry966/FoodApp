bd4a29fb0ef67d9f0d6427af9763a441dd8a626b
richa.tripathi <richa.tripathi@cmssl.in>
2025-08-06 18:45:09 +0530

    refactor: Remove unnecessary React imports from CategoryPage, NonVeg, and Veg components

--- src/pages/CategoryPage.tsx
+++ src/pages/CategoryPage.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import featuredData from "../assets/json/FeaturedItems.json";
 import categoriesData from "../assets/json/FoodCategories.json";
